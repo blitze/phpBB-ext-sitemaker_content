ee2cdec9b0628e5adda8f6ef003206f6647ea80c
blitze <dachebodt@gmail.com>
2014-12-24 15:20:37 +0000

    Allow multiple editors on one page

--- develop/scripts/content_posting.js
+++ develop/scripts/content_posting.js
@@ -2,12 +2,21 @@
 	'use strict';
 
 	$(document).ready(function() {
-		var slug = $('#topic-slug');
-		$('#topic-subject').on('blur keyup change click', function(e) {
-			var title = $(this).val().toLowerCase()
-				.replace(/[^\w ]+/g, '')
-				.replace(/ +/g, '-');
-			slug.val(title);
+		$('.color_palette').each(function() {
+			var el = $(this);
+			var	orientation	= el.data('orientation');
+			var height		= el.data('height');
+			var width		= el.data('width');
+			var target		= el.data('target');
+			var bbcode		= el.data('bbcode');
+
+			// Insert the palette HTML into the container.
+			el.html(phpbb.colorPalette(orientation, width, height));
+
+		}).filter(':gt(0)').on('click', 'a', function(e) {
+			// Attach event handler when a palette cell is clicked.
+			var color = $(this).attr('data-color');
+			bbfontstyle('[color=#' + color + ']', '[/color]');
 		});
 	});
 })(jQuery, window, document);
