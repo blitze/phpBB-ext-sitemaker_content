a1ed12224d938cae66b5e317a30e3b6e4288a01b
blitze <dachebodt@gmail.com>
2014-12-28 16:18:20 +0000

    Improve regex to get data from posts

--- services/types.php
+++ services/types.php
@@ -147,7 +147,7 @@ class types
 		$fields_data = array();
 		$find_tags = join('|', $fields);
 
-		if (preg_match_all("/<div data-field=\"($find_tags)\">(.*?)<\/div>/is", $post_text, $matches))
+		if (preg_match_all('/<div[^>]+data-field="(' . $find_tags . ')"[^>]*>(.*)<\/div>/is', $post_text, $matches))
 		{
 			$fields_data = array_combine($matches[1], $matches[2]);
 		}
