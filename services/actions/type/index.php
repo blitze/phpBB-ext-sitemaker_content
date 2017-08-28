c5327c3ed543df822afc261c035088718f4a9e7f
blitze <dachebodt@gmail.com>
2017-08-28 18:00:28 +0000

    Correctly point to posting form

--- services/actions/type/index.php
+++ services/actions/type/index.php
@@ -80,7 +80,7 @@ class index implements action_interface
 				'U_EDIT'		=> $u_action . '&amp;do=edit&amp;type=' . $type,
 				'U_STATUS'		=> $u_action . '&amp;do=toggle_status&amp;type=' . $type,
 				'U_VIEW'		=> $this->controller_helper->route('blitze_content_index', array('type' => $type)),
-				'U_POST'		=> append_sid("{$this->phpbb_root_path}ucp." . $this->php_ext, "i=-blitze-content-ucp-content_module&amp;mode=content&amp;action=post&amp;type={$type}"),
+				'U_POST'		=> append_sid("{$this->phpbb_root_path}posting." . $this->php_ext, "mode=post&amp;f=$forum_id"),
 				'U_GROUP_PERMS'	=> append_sid("{$this->phpbb_admin_path}index." . $this->php_ext, "i=acp_permissions&amp;mode=setting_group_global"),
 				'U_FORUM_PERMS'	=> append_sid("{$this->phpbb_admin_path}index." . $this->php_ext, "i=acp_permissions&amp;mode=setting_forum_local&amp;forum_id[]=$forum_id"),
 			);
