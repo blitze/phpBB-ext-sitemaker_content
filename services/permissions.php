cd2eb2f84dc1cb98611f90632e290426ef4a21e6
blitze <dachebodt@gmail.com>
2018-02-28 06:02:23 +0000

    Improve code quality

--- services/permissions.php
+++ services/permissions.php
@@ -49,8 +49,10 @@ class permissions
 	 */
 	public function permanent_delete_allowed(array $post_data)
 	{
-		return ($this->auth->acl_get('m_delete', $post_data['forum_id']) ||
-		($this->auth->acl_get('f_delete', $post_data['forum_id']) && $this->user->data['user_id'] == $post_data['poster_id']));
+		return (
+			$this->auth->acl_get('m_delete', $post_data['forum_id']) ||
+			($this->auth->acl_get('f_delete', $post_data['forum_id']) && $this->user_is_poster($post_data['poster_id']))
+		);
 	}
 
 	/**
@@ -114,11 +116,112 @@ class permissions
 	 */
 	protected function edit_allowed(array $post_data, array $topic_data)
 	{
-		return ($this->user->data['is_registered'] && ($this->auth->acl_get('m_edit', $post_data['forum_id']) || (
-			!$this->cannot_edit($post_data) &&
-			!$this->cannot_edit_time($post_data) &&
-			!$this->cannot_edit_locked($post_data, $topic_data)
-		)));
+		return $this->auth->acl_get('m_edit', $post_data['forum_id']) || !$this->user_cannot_modify_post($post_data, $topic_data, 'edit');
+	}
+
+	/**
+	 * @param array $post_data
+	 * @param array $topic_data
+	 * @return bool
+	 */
+	protected function delete_allowed(array $post_data, array $topic_data)
+	{
+		return $this->moderator_can_delete($post_data, $topic_data) || (
+			!$this->cannot_delete_lastpost($post_data, $topic_data) &&
+			!$this->user_cannot_modify_post($post_data, $topic_data, 'delete')
+		);
+	}
+
+	/**
+	 * @param array $post_data
+	 * @param array $topic_data
+	 * @return bool
+	 */
+	protected function moderator_can_delete(array $post_data, array $topic_data)
+	{
+		return (
+			$this->auth->acl_get('m_delete', $post_data['forum_id']) ||
+			($this->auth->acl_get('m_softdelete', $post_data['forum_id']) && $post_data['post_visibility'] != ITEM_DELETED)
+		);
+	}
+
+	/**
+	 * @param array $post_data
+	 * @param array $topic_data
+	 * @param string $mode
+	 * @return bool
+	 */
+	protected function user_cannot_modify_post(array $post_data, array $topic_data, $mode)
+	{
+		$callable = 'cannot_' . $mode;
+		return $this->$callable($post_data) &&
+			!$this->cannot_modify_time($post_data, $mode) &&
+			!$this->cannot_modify_locked($post_data, $topic_data);
+	}
+
+	/**
+	 * @param array $post_data
+	 * @return bool
+	 */
+	protected function softdelete_allowed(array $post_data)
+	{
+		return (
+			$this->auth->acl_get('m_softdelete', $post_data['forum_id']) ||
+			($this->auth->acl_get('f_softdelete', $post_data['forum_id']) && $this->user_is_poster($post_data['poster_id']))
+		) && $post_data['post_visibility'] != ITEM_DELETED;
+	}
+
+	/**
+	 * @param array $post_data
+	 * @param string $mode edit|delete
+	 * @return bool
+	 */
+	protected function cannot_modify_time(array $post_data, $mode)
+	{
+		$mode += '_time';
+		return $this->config[$mode] && $post_data['post_time'] <= time() - ($this->config[$mode] * 60);
+	}
+
+	/**
+	 * we do not want to allow removal of the last post if a moderator locked it!
+	 * @param array $post_data
+	 * @param array $topic_data
+	 * @return bool
+	 */
+	protected function cannot_modify_locked(array $post_data, array $topic_data)
+	{
+		return $topic_data['topic_status'] == ITEM_LOCKED || $post_data['post_edit_locked'];
+	}
+
+	/**
+	 * @param array $post_data
+	 * @return bool
+	 */
+	protected function cannot_edit(array $post_data)
+	{
+		return !$this->user_is_poster($post_data['poster_id']) || !$this->auth->acl_get('f_edit', $post_data['forum_id']);
+	}
+
+	/**
+	 * @param array $post_data
+	 * @return bool
+	 */
+	protected function cannot_delete(array $post_data)
+	{
+		return !$this->user_is_poster($post_data['poster_id']) || (
+			!$this->auth->acl_get('f_delete', $post_data['forum_id']) &&
+			(!$this->auth->acl_get('f_softdelete', $post_data['forum_id']) || $post_data['post_visibility'] == ITEM_DELETED)
+		);
+	}
+
+	/**
+	 * @param array $post_data
+	 * @param array $topic_data
+	 * @return bool
+	 */
+	protected function cannot_delete_lastpost(array $post_data, array $topic_data)
+	{
+		return $topic_data['topic_last_post_id'] == $post_data['post_id'];
 	}
 
 	/**
@@ -140,100 +243,5 @@ class permissions
 	protected function post_is_quotable(array $post_data, array $topic_data)
 	{
 		return ($post_data['post_visibility'] == ITEM_APPROVED && $topic_data['topic_first_post_id'] != $post_data['post_id']);
-	}
-
-	/**
-	 * @param array $post_data
-	 * @param array $topic_data
-	 * @return bool
-	 */
-	protected function delete_allowed(array $post_data, array $topic_data)
-	{
-		return ($this->user->data['is_registered'] && (($this->auth->acl_get('m_delete', $post_data['forum_id']) || ($this->auth->acl_get('m_softdelete', $post_data['forum_id']) && $post_data['post_visibility'] != ITEM_DELETED)) || (
-			!$this->cannot_delete($post_data) &&
-			!$this->cannot_delete_lastpost($post_data, $topic_data) &&
-			!$this->cannot_delete_time($post_data) &&
-			!$this->cannot_delete_locked($post_data, $topic_data)
-		)));
-	}
-
-	/**
-	 * @param array $post_data
-	 * @return bool
-	 */
-	protected function softdelete_allowed(array $post_data)
-	{
-		return ($this->auth->acl_get('m_softdelete', $post_data['forum_id']) ||
-			($this->auth->acl_get('f_softdelete', $post_data['forum_id']) && $this->user->data['user_id'] == $post_data['poster_id'])) && ($post_data['post_visibility'] != ITEM_DELETED);
-	}
-
-	/**
-	 * @param array $post_data
-	 * @return bool
-	 */
-	protected function cannot_edit(array $post_data)
-	{
-		return (!$this->auth->acl_get('f_edit', $post_data['forum_id']) || $this->user->data['user_id'] != $post_data['poster_id']);
-	}
-
-	/**
-	 * @param array $post_data
-	 * @return bool
-	 */
-	protected function cannot_edit_time(array $post_data)
-	{
-		return ($this->config['edit_time'] && $post_data['post_time'] <= time() - ($this->config['edit_time'] * 60));
-	}
-
-	/**
-	 * @param array $post_data
-	 * @param array $topic_data
-	 * @return bool
-	 */
-	protected function cannot_edit_locked(array $post_data, array $topic_data)
-	{
-		return ($topic_data['topic_status'] == ITEM_LOCKED || $post_data['post_edit_locked']);
-	}
-
-	/**
-	 * @param array $post_data
-	 * @return bool
-	 */
-	protected function cannot_delete(array $post_data)
-	{
-		return $this->user->data['user_id'] != $post_data['poster_id'] || (
-			!$this->auth->acl_get('f_delete', $post_data['forum_id']) &&
-			(!$this->auth->acl_get('f_softdelete', $post_data['forum_id']) || $post_data['post_visibility'] == ITEM_DELETED)
-		);
-	}
-
-	/**
-	 * @param array $post_data
-	 * @param array $topic_data
-	 * @return bool
-	 */
-	protected function cannot_delete_lastpost(array $post_data, array $topic_data)
-	{
-		return $topic_data['topic_last_post_id'] != $post_data['post_id'];
-	}
-
-	/**
-	 * @param array $post_data
-	 * @return bool
-	 */
-	protected function cannot_delete_time(array $post_data)
-	{
-		return $this->config['delete_time'] && $post_data['post_time'] <= time() - ($this->config['delete_time'] * 60);
-	}
-
-	/**
-	 * we do not want to allow removal of the last post if a moderator locked it!
-	 * @param array $post_data
-	 * @param array $topic_data
-	 * @return bool
-	 */
-	protected function cannot_delete_locked(array $post_data, array $topic_data)
-	{
-		return $topic_data['topic_status'] == ITEM_LOCKED || $post_data['post_edit_locked'];
 	}
 }
